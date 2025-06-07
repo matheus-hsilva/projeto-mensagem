d31ae076074551ab76c0265658b8dfe6676d0ac2
matheus-hsilva <m4751376@gmail.com>
2025-06-07 14:00:39 -0300

    Adicionando arquivos do repositório site-mensagem

---
+++ src/javascript/script.js
@@ -0,0 +1,152 @@
+$(document).ready(function () {
+  ScrollReveal().reveal('.hero-text', {
+    scale: 0.87,
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.next-text', {
+    scale: 0.87,
+    delay: 150,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.card-item', {
+    scale: 0.87,
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.intro-image', {
+    origin: 'left',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.intro-image-mb', {
+    origin: 'left',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.intro-content', {
+    origin: 'right',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.transition-button', {
+    scale: 0.87,
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.transition-title', {
+    origin: 'right',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.galery-title', {
+    origin: 'right',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.galery-photos img', {
+    scale: 0.87,
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.audio-title', {
+    origin: 'right',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.audio-player', {
+    scale: 0.87,
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.faith-image', {
+    origin: 'left',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.faith-image-mb', {
+    origin: 'left',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.faith-content', {
+    origin: 'right',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+
+  ScrollReveal().reveal('.spotify-title', {
+    origin: 'right',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.spotify-player', {
+    scale: 0.87,
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.message-description', {
+    origin: 'left',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+
+  ScrollReveal().reveal('.galery-travel-title', {
+    origin: 'right',
+    distance: '10%',
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.galery-travel-photos img', {
+    scale: 0.87,
+    delay: 200,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.flower-red-1, .flower-red-2', {
+    origin: 'bottom',
+    distance: '10%',
+    delay: 150,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.flower-yellow-1, .flower-yellow-2, .flower-blue-1, .flower-blue-2', {
+    scale: 0.87,
+    delay: 150,
+    duration: 1200
+  });
+  
+  ScrollReveal().reveal('.image-blob-1', {
+    scale: 0.87,
+    delay: 150,
+    duration: 1200
+  });
+});
